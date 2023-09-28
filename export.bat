31c9d8b2515a7c901dc0d78c55127a2cc745942d
techtanic <techtanic@outlook.com>
2023-09-28 17:12:31 +0530

    Website updates.

--- export.bat
+++ export.bat
@@ -1,5 +1,5 @@
-@ECHO  OFF
-CD %1 
+@ECHO OFF
+CD %1
 setlocal enabledelayedexpansion
 
 set "search=http://127.0.0.1:5000"
@@ -7,15 +7,13 @@ set "replace=https://tedxsac.up.railway.app"
 set "files=index.html admin.html registration.html"
 
 for %%F in (%files%) do (
-    set "input_file=%%F"
-    set "output_file=%%~nF_modified.html"
-
-    for /f "tokens=*" %%A in ('powershell -command "(Get-Content '!input_file!') | ForEach-Object { $_ -replace [regex]::Escape('%search%'), '%replace%' } | Set-Content '!output_file!'"') do (
+    for /f "tokens=*" %%A in ('powershell -command "(Get-Content '%%F') | ForEach-Object { $_ -replace [regex]::Escape('%search%'), '%replace%' } | Set-Content '%%F'"') do (
         echo %%A
     )
 
-    echo Replacement complete for !input_file!. Modified output saved to !output_file!
+    echo Replacement complete for %%F!
 )
+
 git add -A
 git commit -m "Website updates." 
 git push origin main
