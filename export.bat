d9d4355137619ba802fece78877e789d96d86e6e
techtanic <techtanic@outlook.com>
2023-09-28 17:18:27 +0530

    Website updates.

--- export.bat
+++ export.bat
@@ -6,12 +6,17 @@ set "search=http://127.0.0.1:5000"
 set "replace=https://tedxsac.up.railway.app"
 set "files=index.html admin.html registration.html"
 
+echo Debug: Search string is "%search%"
+echo Debug: Replace string is "%replace%"
+
 for %%F in (%files%) do (
+    echo Debug: Processing file %%F...
+
     for /f "tokens=*" %%A in ('powershell -command "(Get-Content '%%F') | ForEach-Object { $_ -replace [regex]::Escape('%search%'), '%replace%' } | Set-Content '%%F'"') do (
-        echo %%A
+        echo Debug: Replacing in file %%F: %%A
     )
 
-    echo Replacement complete for %%F!
+    echo Debug: Replacement complete for %%F!
 )
 
 git add -A
