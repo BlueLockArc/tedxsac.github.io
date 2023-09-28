d880ef0db6c4e6637f8b143c323d17ef5d5e5b5a
techtanic <techtanic@outlook.com>
2023-09-28 17:24:37 +0530

    Website updates.

--- assets/js/test.js
+++
@@ -1,4 +0,0 @@
-function showhide(show,hide) {
-            document.getElementById(show).classList.remove("toggleable");
-            document.getElementById(hide).classList.add("toggleable");
-        }
