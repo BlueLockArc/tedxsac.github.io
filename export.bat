960fe13823f29d5e58413aff26af7dc2f6c6e9ac
techtanic <techtanic@outlook.com>
2023-09-28 17:29:27 +0530

    efef

--- export.bat
+++ export.bat
@@ -6,19 +6,15 @@ set "search=http://127.0.0.1:5000"
 set "replace=https://tedxsac.up.railway.app"
 set "files=assets/js/main.min.js assets/js/Display-table.min.js"
 
-echo Debug: Search string is "%search%"
-echo Debug: Replace string is "%replace%"
-
+REM Loop through each file
 for %%F in (%files%) do (
-    echo Debug: Processing file %%F...
-
+    REM Use PowerShell to perform search and replace
     for /f "tokens=*" %%A in ('powershell -command "(Get-Content '%%F') | ForEach-Object { $_ -replace [regex]::Escape('%search%'), '%replace%' } | Set-Content '%%F'"') do (
-        echo Debug: Replacing in file %%F: %%A
+        REM No debug output in this version
     )
-
-    echo Debug: Replacement complete for %%F!
 )
 
-git add -A > NULL
-git commit -m "Website updates." > NULL
+REM Stage, commit, and push changes using Git
+git add -A > NUL
+git commit -m "Website updates." > NUL
 git push origin main > NUL
